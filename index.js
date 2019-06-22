2253bb6e5a6126f51e496544ce08a2172c661ab8
Vitalij <Sot00@ukr.net>
2019-06-22 11:24:17 +0300

    Firebase v1

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const Telegraf  = require('telegraf');
 const getSchedule = require('./parse');
+const firebase = require('firebase');
 
+var serviceAccount = require("./accountdb.json")
+
+firebase.initializeApp({
+        serviceAccount: serviceAccount,
+        databaseURL: "https://shedulebot-eb34e.firebaseio.com"
+});
+
+var ref = firebase.database().ref().child('messages');
 const bot = new Telegraf('852884645:AAGe3XpxD85OA8J2xntabUB6JYlRlDje40Y');
 
-bot.start((ctx) => ctx.reply('Hello! enter "get"'));
+bot.start((ctx) => ctx.reply('Enter "get"'));
 bot.hears( 'get',(ctx) => getSchedule(ctx.message.text).then(result => {
+        ref.push({time: new Date().toString(), text: result});
         ctx.reply(result);
 }).catch(() => ctx.reply('Error occured')));
 
